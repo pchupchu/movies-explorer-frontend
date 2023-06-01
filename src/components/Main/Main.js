bf4ecd8005f8622d88bd6b01e8b78f4b89d60e81
Marina Malyarchuk <pchupchu@yandex.ru>
2023-06-01 22:46:30 +0300

    Add Footer

--- src/components/Main/Main.js
+++ src/components/Main/Main.js
@@ -4,6 +4,7 @@ import AboutProject from "./AboutProject/AboutProject";
 import "./Main.css";
 import Techs from "./Techs/Techs";
 import AboutMe from "./AboutMe/AboutMe";
+import Footer from "../Footer/Footer";
 
 function Main() {
   return (
@@ -13,6 +14,7 @@ function Main() {
       <AboutProject />
       <Techs />
       <AboutMe />
+      <Footer />
     </section>
   );
 }
