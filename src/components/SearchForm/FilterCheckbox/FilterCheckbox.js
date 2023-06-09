3cfce17c90ee6c7068350694905cbe07164ceb2a
Marina Malyarchuk <pchupchu@yandex.ru>
2023-06-09 16:29:16 +0300

    Add state to FilterCheckbox

--- src/components/SearchForm/FilterCheckbox/FilterCheckbox.js
+++ src/components/SearchForm/FilterCheckbox/FilterCheckbox.js
@@ -1,6 +1,13 @@
 import "./FilterCheckbox.css";
+import { useState } from "react";
 
 function FilterCheckbox() {
+  const [isShortFilm, setIsShortFilm] = useState(false);
+
+  function handleShortFilm() {
+    setIsShortFilm(!isShortFilm);
+  }
+
   return (
     <div className="filter">
       <input
@@ -8,6 +15,7 @@ function FilterCheckbox() {
         className="filter__input"
         id="short-film"
         name="short-film"
+        onClick={handleShortFilm}
       />
       <label htmlFor="short-film" className="filter__title">
         Короткометражки
